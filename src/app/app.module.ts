8932597527e73559204b36e14894dd5a3d256d59
wdwf <w_dwf@hotmail.com>
2021-03-25 18:16:48 -0300

    Finalização do exercicio 5

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import{FormsModule} from '@angular/forms'
+import{FormsModule} from '@angular/forms';
+
 import{MatInputModule} from '@angular/material/input';
 import{MatCardModule}from'@angular/material/card';
 import{MatButtonModule} from '@angular/material/button';
@@ -12,13 +13,14 @@ import { BodycComponent } from './bodyc/bodyc.component';
 import { CardlComponent } from './cardl/cardl.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { LivroService } from './cardl/livro.service';
+
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     BodycComponent,
     CardlComponent,
-
   ],
   imports: [
     BrowserModule,
@@ -29,7 +31,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     MatButtonModule,
     MatExpansionModule,
   ],
-  providers: [],
+  providers: [LivroService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/cardl/cardl.component.ts
+++ src/app/cardl/cardl.component.ts
@@ -1,11 +1,32 @@
-import { Component,Input} from '@angular/core';
-import {Book} from '../cardl/book.model';
+import { Component, OnInit ,OnDestroy} from '@angular/core';
+import { Livro } from './livro.model';
+import { LivroService } from '../cardl/livro.service';
+import { Subscription, Observable } from 'rxjs';
 
 @Component({
   selector: 'app-cardl',
   templateUrl: './cardl.component.html',
   styleUrls: ['./cardl.component.css']
 })
-export class CardlComponent  {
-  @Input() collection:Book[]=[]
+
+export class CardlComponent implements OnInit, OnDestroy {
+
+  collection: Livro[] = [];
+  private collectionsSubscription: Subscription;
+
+  constructor(public livroService: LivroService ) {}
+
+  ngOnDestroy(): void {
+    this.collectionsSubscription.unsubscribe();
+  }
+
+  ngOnInit(): void {
+    this.collection = this.livroService.getLivros();
+    this.collectionsSubscription = this.livroService
+    .getListaDeLivrosAtualizadaObservable()
+    .subscribe((collection: Livro[]) => {
+      this.collection = collection;
+    });
+  }
+
 }
